84b2398f08a663d87420d0f7b4b6640f416f8709
HPPinata <83947761+HPPinata@users.noreply.github.com>
2023-08-28 16:16:56 +0200

    correct installation links

--- pelicanconf.py
+++ pelicanconf.py
@@ -34,7 +34,7 @@ MENUITEMS = (('FAQ', 'https://github.com/PrivateBin/PrivateBin/wiki/FAQ'),
              ('Find instance', '/directory/'))
 
 # Blogroll
-LINKS = (('Installation guide', 'https://github.com/PrivateBin/PrivateBin/blob/master/INSTALL.md#installation'),
+LINKS = (('Installation guide', 'https://github.com/PrivateBin/PrivateBin/blob/master/doc/Installation.md'),
          ('Configuration guide', 'https://github.com/PrivateBin/PrivateBin/wiki/Configuration'),
          ('Template guide', 'https://github.com/PrivateBin/PrivateBin/wiki/Templates'),
          ('Upgrading from ZeroBin 0.19 Alpha', 'https://github.com/PrivateBin/PrivateBin/wiki/Upgrading-from-ZeroBin-0.19-Alpha'),
